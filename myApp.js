68bdd9e93005fb793a9d93215bdd2097450bf6f5
taufan <taufan@vecapital.asia>
2023-08-21 20:09:45 +0700

    create a schema and model person

--- myApp.js
+++ myApp.js
@@ -4,8 +4,16 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 
-let Person;
+const Schema = mongoose.Schema;
 
+const personSchema = new Schema({
+  name: {type: String, required: true},
+  age: {type: Number},
+  favoriteFoods: [String]
+});
+
+
+const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
