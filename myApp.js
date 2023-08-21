d20509819031fe9c7c2a6cf060a7bfb86221e9fe
taufan <taufan@vecapital.asia>
2023-08-21 19:55:05 +0700

    bugfix

--- myApp.js
+++ myApp.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
-const mongoose = require('mongoose').config();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 
 let Person;
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
