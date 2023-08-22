4a62abe418c130e9a296b4ca86e7cf6035757a1c
taufan <taufan@vecapital.asia>
2023-08-22 11:39:29 +0700

    fix

--- myApp.js
+++ myApp.js
@@ -101,7 +101,7 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-  Person.find({favoriteFoods: foodToSearch}).sort({name: -1}).limit(2).select({age: 0}).exec((err, document)=>{
+  Person.find({ favoriteFoods: foodToSearch }).sort({ name: 1 }).limit(2).select({ age: 0 }).exec((err, document)=>{
     if (err) return console.log(err);
     done(null, document);
   });
