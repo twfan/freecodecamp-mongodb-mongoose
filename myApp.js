19c4fef9ec7db73098336bdd5b4a84930be9d6c1
taufan <taufan@vecapital.asia>
2023-08-22 10:01:42 +0700

    push find

--- myApp.js
+++ myApp.js
@@ -42,7 +42,10 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name: personName}, (err, personFound)=>{
+    if (err) return console.log(err);
+    done(null, personFound);
+  });
 };
 
 const findOneByFood = (food, done) => {
